4a173fa0a7a3dc4770ced8566df6d3404f363ce7
@LaibaAshfaq2 <165155950+laibaashfaq1@users.noreply.github.com>
2024-10-28 10:02:41 +0500

    Navbar.tsx

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -65,53 +65,7 @@ const Navbar = () => {
 export default Navbar;
 
 
-// 'use client';
-// import React from 'react';
 
-// const Navbar: React.FC = () => {
-//   const scrollToSection = (id: string) => {
-//     const section = document.getElementById(id);
-//     if (section) {
-//       section.scrollIntoView({ behavior: 'smooth' });
-//     }
-//   };
 
-//   return (
-//     <nav className="w-full flex justify-between items-center px-8 py-4 bg-gray-900 border-b-2 border-gray-200">
-//       {/* Brand Logo */}
-//       <div className="text-2xl font-bold text-white">Laiba</div>
-
-//       {/* Navigation Links */}
-//       <div className="flex items-center space-x-8">
-//         <ul className="flex space-x-8 text-white">
-//           <li className="menulink cursor-pointer hover:text-yellow-400 transition">
-//             <a onClick={() => scrollToSection('home-section')}>Home</a>
-//           </li>
-//           <li className="menulink cursor-pointer hover:text-yellow-400 transition">
-//             <a onClick={() => scrollToSection('about-section')}>About Me</a>
-//           </li>
-//           <li className="menulink cursor-pointer hover:text-yellow-400 transition">
-//             <a onClick={() => scrollToSection('services-section')}>Services</a>
-//           </li>
-//           <li className="menulink cursor-pointer hover:text-yellow-400 transition">
-//             <a onClick={() => scrollToSection('portfolio-section')}>Portfolio</a>
-//           </li>
-//         </ul>
-
-//         {/* Contact Button */}
-//         <button
-//           className="border-2 border-yellow-400 text-white px-4 py-2 rounded-lg transition duration-300 
-//             hover:bg-yellow-400 hover:text-black"
-//           aria-label="Contact"
-//           onClick={() => scrollToSection('contact-section')}
-//         >
-//           Contact
-//         </button>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
 
 
